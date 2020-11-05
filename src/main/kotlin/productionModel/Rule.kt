370f9cb744e564d071362c5673e51364156a7193
Oxana <lereenadem@yandex.ru>
2020-11-05 15:37:50 +0300

    Добавлено многое для продукционной модели

--- src/main/kotlin/productionModel/Rule.kt
+++ src/main/kotlin/productionModel/Rule.kt
@@ -5,6 +5,6 @@ class Rule(val antecedents: List<String>, val consequent: String, val final: Boo
         var antenc = antecedents[0]
         for (i in 1 until antecedents.size)
             antenc += "," + antecedents[i]
-        return (if (final) "Final:" else "") + " -> " + consequent
+        return antecedents.joinToString(", ") + " -> " + consequent
     }
 }
--- src/main/kotlin/productionModel/main.kt
+++ src/main/kotlin/productionModel/main.kt
@@ -3,8 +3,8 @@ package productionModel
 import java.io.File
 
 fun main() {
-    val rulesFile = File("resources/ruleBase.txt").readLines()
-    val ruleBase = parseRules(rulesFile)
+    val rulesFile = File("src/main/resources/rulesBase.txt").readLines()
+    val ruleBase = parseRules(rulesFile).toMutableList()
 //    val finalStates = File("resources/finalStates.txt").readLines().toHashSet()
 
     println("Forward (0) или backward (1)?")
@@ -14,7 +14,8 @@ fun main() {
         searchType = readLine()!!.toInt()
     }
     if (searchType == 0) {
-        println("Введите факты через двоеточие:")
+        println("Выберите с чем вы пойдёте в приключение: Деньги, Щит, Оружие, Латы, Дети, Отвага")
+        println("Введите выбранное через двоеточие:")
         var factsLine = readLine()
         while (factsLine == null || factsLine.isEmpty()) {
             println("Некорректные факты, попробуйте ещё раз")
@@ -24,9 +25,10 @@ fun main() {
         for (fact in factsLine.split(':'))
             facts.add(fact.trim())
 
-        println(forwardPass(facts, ruleBase))
+        val path = forwardPass(facts, ruleBase)
+        println(path)
     } else {
-        val facts = File("resources/facts.txt").readLines().toHashSet()
+        val facts = File("src/main/resources/facts.txt").readLines().toHashSet()
         println("Введите результат для поиска:")
         var fact = readLine()
         while (fact == null || fact.isEmpty()) {
@@ -34,88 +36,96 @@ fun main() {
             fact = readLine()
         }
 
-        println(backwardPass(fact, ruleBase, facts))
+//        println(backwardPass(fact, ruleBase, facts))
     }
 }
 
-fun forwardPass(facts: HashSet<String>, rules: List<Rule>): String {
-    var currentSet = HashSet<Node>()
-    currentSet.add(Node(facts, null, null))
-    val final = rules.asSequence().filter { x -> x.final }
-
-    for (target in final) {
-        var targetNode: Node? = null
-        while (targetNode == null) {
-            val nextSet = HashSet<Node>()
-            for (node in currentSet) {
-                for (rule in rules) {
-                    if (rule.antecedents.any { fact -> !node.set.contains(fact) })
-                        continue
-                    val newNode = Node(HashSet(node.set), node, rule)
-                    newNode.set.add(rule.consequent)
-                    nextSet.add(newNode)
-                    if (newNode.set.contains(target.consequent))
-                        targetNode = newNode
-                }
+fun forwardPass(facts: HashSet<String>, rules: MutableList<Rule>): String {
+    var currentNode = Node(facts, null, null)
+    val targetNode: Node?
+    while (true) {
+        var ruleToRemove: Rule? = null
+        for (rule in rules) {
+            if (!facts.containsAll(rule.antecedents)) {
+                if (rule == rules.last())
+                    break
+                continue
             }
-            if (currentSet == nextSet)
-                break
-            currentSet = nextSet
+            facts.add(rule.consequent)
+            ruleToRemove = rule
+            currentNode = Node(facts, currentNode, rule)
+            break
         }
-        if (targetNode == null)
-            return "Не найдено"
-
+        if (ruleToRemove != null)
+            rules.remove(ruleToRemove)
+        if (rules.any { rule -> facts.containsAll(rule.antecedents) })
+            continue
+        targetNode = currentNode
+        if (targetNode.parent == null)
+            return "С таким набором ничего не выйдет"
         return inference(targetNode)
     }
-
-    return "Не найдено"
 }
 
-fun backwardPass(fact: String, rules: List<Rule>, facts: HashSet<String>): String {
-    var currentSet = HashSet<Node>()
-    val initial = HashSet<String>()
-    initial.add(fact)
-    currentSet.add(Node(initial, null, null))
-
-    var sourceNode: Node? = null
-    while (true) {
-        val nextSet = HashSet<Node>()
-        for (node in currentSet) {
-            val activeRules =
-                rules.filter { rule -> !facts.contains(rule.consequent) && node.set.contains(rule.consequent) }
-            for (rule in activeRules) {
-                val set = HashSet<String>(node.set)
-                set.remove(rule.consequent)
-                for (antecedent in rule.antecedents)
-                    set.add(antecedent)
-                val newNode = Node(set, node, rule)
-                nextSet.add(newNode)
-                if (set.all { f -> facts.contains(f) }) {
-                    sourceNode = newNode
-//                    if (sourceNode == null)
-//                        return "Вывод невозможен"
-                    return inference(sourceNode)
-                }
-                if (currentSet == nextSet)
-                    break
-                currentSet = nextSet
-            }
-        }
-        if (sourceNode == null)
-            return "Вывод невозможен"
-        return inference(sourceNode)
-    }
-}
+//fun backwardPass(fact: String, rules: List<Rule>, facts: HashSet<String>): String {
+//    var currentSet = HashSet<Node>()
+//    val initial = HashSet<String>()
+//    initial.add(fact)
+//    currentSet.add(Node(initial, null, null))
+//
+//    var sourceNode: Node? = null
+//    while (true) {
+//        val nextSet = HashSet<Node>()
+//        for (node in currentSet) {
+//            val activeRules =
+//                rules.filter { rule -> !facts.contains(rule.consequent) && node.set.contains(rule.consequent) }
+//            for (rule in activeRules) {
+//                val set = HashSet<String>(node.set)
+//                set.remove(rule.consequent)
+//                for (antecedent in rule.antecedents)
+//                    set.add(antecedent)
+//                val newNode = Node(set, node, rule)
+//                nextSet.add(newNode)
+//                if (set.all { f -> facts.contains(f) }) {
+//                    sourceNode = newNode
+//                    return inference(sourceNode)
+//                }
+//                if (currentSet == nextSet)
+//                    break
+//                currentSet = nextSet
+//            }
+//        }
+//        if (sourceNode == null)
+//            return "Вывод невозможен"
+//        return inference(sourceNode)
+//    }
+//}
+//
+//fun backwardPass(aimFact: String, rules: List<Rule>, facts: HashSet<String>): String {
+//    val allGainedFacts = HashSet<String>()
+//    val currentFacts = ArrayList<String>()
+//    currentFacts.add(aimFact)
+//
+//    var currentNode: Node? = null
+//    var sourceNode: Node? = null
+//    for (rule in rules) {
+//        for (fact in currentFacts) {
+//            if (rule.consequent == fact)
+//                currentNode = Node(allGainedFacts, currentNode, rule)
+//            break
+//        }
+//    }
+//}
 
 fun inference(targetNode: Node?): String {
-    var targetNode = targetNode
+    var node = targetNode
     val inference = ArrayList<Rule>()
-    while (targetNode?.parent != null) {
-        inference.add(targetNode.rule!!)
-        targetNode = targetNode.parent
+    while (node?.parent != null) {
+        inference.add(node.rule!!)
+        node = node.parent
     }
     inference.reverse()
-    return inference.joinToString { "\n\r" }
+    return inference.joinToString("\n\r")
 }
 
 fun parseRules(rulesFile: List<String>): List<Rule> {
